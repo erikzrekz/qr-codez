a6d56dab495d3efb51a8127e2afd8f2175408b4e
Eriks <eriksreks@gmail.com>
2017-01-31 01:40:34 -0500

    Inital commit * Working node app * ipfs client * Usage of qrious library for QR code generation * public/main.js mostly uses ajax but also starts to explore how indexdb can be used for hash/data storage

--- app.js
+++ app.js
@@ -1,66 +1,24 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const pug = require('pug')
+const ipfs = require('./lib/ipfs')
 
-/**
- * Module dependencies
- */
+const app = express()
+app.set('port', 4000)
 
-var express = require('express'),
-  bodyParser = require('body-parser'),
-  methodOverride = require('method-override'),
-  errorHandler = require('error-handler'),
-  morgan = require('morgan'),
-  routes = require('./routes'),
-  api = require('./routes/api'),
-  http = require('http'),
-  path = require('path');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-var app = module.exports = express();
+const html = pug.renderFile('./templates/index.pug.html')
 
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
-/**
- * Configuration
- */
+app.get('/', (req, res) => res.send(html))
+app.post('/ipfs', ipfs.addFile)
+app.get('/ipfs/:hash', ipfs.getFile)
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(morgan('dev'));
-app.use(bodyParser());
-app.use(methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
+const httpServer = http.createServer(app);
+httpServer.listen(app.get('port'), () => ipfs.goOnline())
 
-var env = process.env.NODE_ENV || 'development';
-
-// development only
-if (env === 'development') {
-  app.use(express.errorHandler());
-}
-
-// production only
-if (env === 'production') {
-  // TODO
-}
-
-
-/**
- * Routes
- */
-
-// serve index and view partials
-app.get('/', routes.index);
-app.get('/partials/:name', routes.partials);
-
-// JSON API
-app.get('/api/name', api.name);
-
-// redirect all others to the index (HTML5 history)
-app.get('*', routes.index);
-
-
-/**
- * Start Server
- */
-
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
